3c02eaf99a0370d1bb90569fa27421531e402ece
SantiagoRamEs <ramirezsantiago2503@gmail.com>
2026-02-18 15:56:55 -0500

    Update adapters.py

--- professors_fqiq/adapters.py
+++ professors_fqiq/adapters.py
@@ -4,16 +4,14 @@ from django.shortcuts import redirect
 from django.contrib import messages
 
 class RestrictDomainSocialAccountAdapter(DefaultSocialAccountAdapter):
-
     def pre_social_login(self, request, sociallogin):
         allowed_domain = "unmsm.edu.pe"
-
-        email = sociallogin.account.extra_data.get("email", "").strip().lower()
-
+        
+        # Obtener el email del usuario
+        email = ''
+        email = sociallogin.account.extra_data.get('email', '').strip().lower()
+        
+        # Validar dominio
         if not email.endswith(f"@{allowed_domain}"):
-            messages.error(
-                request,
-                f"Debes iniciar sesión con un correo institucional (@{allowed_domain})."
-            )
-            raise ImmediateHttpResponse(redirect("home"))
-
+            messages.error(request, f"Debes iniciar sesión con un correo institucional (Ejemplo: usuario@{allowed_domain})")
+            raise ImmediateHttpResponse(redirect('home'))
