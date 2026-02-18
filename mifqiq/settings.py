36fd069216151a8452902b5355e0b3fb3c6a594c
SantiagoRamEs <ramirezsantiago2503@gmail.com>
2026-02-18 11:47:07 -0500

    ok

--- mifqiq/settings.py
+++ mifqiq/settings.py
@@ -105,7 +105,10 @@ MIDDLEWARE = [
     'allauth.account.middleware.AccountMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'mifqiq.urls'
 
@@ -173,10 +176,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    BASE_DIR / "static", 
+    os.path.join(BASE_DIR, 'static'),
 ]
+
 
 
 # Default primary key field type
