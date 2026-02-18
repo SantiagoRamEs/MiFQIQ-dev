410f0847ef354563866337b2a4a44f3c945131e2
SantiagoRamEs <ramirezsantiago2503@gmail.com>
2026-02-18 12:19:51 -0500

    Update settings.py

--- mifqiq/settings.py
+++ mifqiq/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Archivos multimedia
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 
 
 
