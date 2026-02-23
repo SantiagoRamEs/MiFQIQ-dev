7e373668f8188a4755cb7e2ca72cd35c8e1e92fb
SantiagoRamEs <ramirezsantiago2503@gmail.com>
2026-02-22 19:56:01 -0500

    supabse-img

--- mifqiq/settings.py
+++ mifqiq/settings.py
@@ -261,3 +261,19 @@ LOGGING = {
     },
 }
 
+#CONEXIÓN DE IMG CON SUPABSE
+INSTALLED_APPS += ["storages"]
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+
+AWS_S3_ENDPOINT_URL = os.environ.get("AWS_S3_ENDPOINT_URL")
+AWS_S3_REGION_NAME = "us-east-2"
+
+AWS_QUERYSTRING_AUTH = False
+AWS_DEFAULT_ACL = "public-read"
+
