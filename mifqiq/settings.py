f8747704e77fe5e4be964a8f6538337e7359c31b
SantiagoRamEs <ramirezsantiago2503@gmail.com>
2026-02-23 01:45:10 -0500

    Revert "Update settings.py"
    This reverts commit 4fc69b8c029053040cb1d0db3be6c0e9817e8d7a.

--- mifqiq/settings.py
+++ mifqiq/settings.py
@@ -97,19 +97,6 @@ INSTALLED_APPS = [
     #app
     'professors_fqiq',
 ]
-
-SOCIALACCOUNT_PROVIDERS = {
-    "google": {
-        "SCOPE": [
-            "profile",
-            "email",
-        ],
-        "AUTH_PARAMS": {
-            "access_type": "online",
-        },
-    }
-}
-
 SITE_ID = 1
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',        # autenticación normal
