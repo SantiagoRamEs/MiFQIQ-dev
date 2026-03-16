e3865d8d27b1bceadd2599e51178a3eb4b45bcc9
SantiagoRamEs <ramirezsantiago2503@gmail.com>
2026-03-15 20:00:49 -0500

    admin.py

--- professors_fqiq/admin.py
+++ professors_fqiq/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from .models import Professor, Grade, Course, ProfessorCourse
 
+class ProfessorCourseInline(admin.TabularInline):
+    model = ProfessorCourse
+    extra = 1
+
+class ProfessorAdmin(admin.ModelAdmin):
+    inlines = [ProfessorCourseInline]
+    list_display = ("name", "get_courses")
+
+    def get_courses(self, obj):
+        return ", ".join([pc.course.name_course for pc in obj.professorcourse_set.all()])
+
+    get_courses.short_description = "Cursos"
+
+
 
 class GradeAdmin(admin.ModelAdmin):
     readonly_fields = ("created",)
@@ -8,9 +22,6 @@ class GradeAdmin(admin.ModelAdmin):
 
 class CourseAdmin(admin.ModelAdmin):
     readonly_fields = ("created",)
-
-class ProfessorAdmin(admin.ModelAdmin):
-    pass
 
 
 admin.site.register(Grade, GradeAdmin)
