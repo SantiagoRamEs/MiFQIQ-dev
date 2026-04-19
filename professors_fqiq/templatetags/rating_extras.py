2341b4f650f68d99b694743da14346e18ea2ca42
Santiago Ramirez <ramirezsantiago2503@gmail.com>
2026-04-19 17:49:04 -0500

    ok

--- professors_fqiq/templatetags/rating_extras.py
+++ professors_fqiq/templatetags/rating_extras.py
@@ -2,6 +2,7 @@ from django import template
 
 register = template.Library()
 
+#RATING
 @register.filter
 def stars(value):
     if value is None:
@@ -9,3 +10,10 @@ def stars(value):
 
     rounded = int(round(value))
     return "★" * rounded + "☆" * (5 - rounded)
+
+#NAMES-COMMENTS
+@register.filter
+def first_word(value):
+    if value:
+        return value.split()[0]
+    return ''
